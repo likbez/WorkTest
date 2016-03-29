3e72b4290b0ef7491e7282d2eb7dd1d531bc3819
likbez <likbez.reg@gmail.com>
2016-03-30 02:17:33 +0400

    one

---
+++ src/main/java/Zaraza.java
@@ -0,0 +1,29 @@
+
+import javax.swing.*;
+import java.awt.Dimension;
+import java.io.IOException;
+
+
+class Zaraza {
+    public static void main(String[] args) throws IOException {
+
+        /*JFrame fr = new JFrame("Me Frame");
+        fr.setSize(new Dimension(600, 400));
+        fr.setLocationRelativeTo(null);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.setVisible(true);
+      */
+
+        byte[] data = new byte[7];
+        String s = "ножницы";
+
+
+        System.out.println("Два кружочка по середине гвоздик: ");
+        System.in.read(data);
+        System.out.flush();
+
+
+
+
+    }
+}
